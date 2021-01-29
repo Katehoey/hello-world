9566dfba5274106af7dd7a686411af273536369a
katehoey11 <kmhoey@loyola.edu>
2021-01-29 10:51:46 -0500

    share

---
+++ Lab 1 B.py
@@ -0,0 +1,13 @@
+#Programmers: Kate Hoey Grace Rapisarda
+#Course: CS151-01
+#January 28 2021
+#Lab 1
+#Program Inputs: Number of ml
+#Program Outputs: Teaspoons and tablespoons
+
+
+
+#Formula for converting ml to teaspoons and tablespoons
+#Ask user for ml
+#output number of teaspoons
+#output number of tablespoons
