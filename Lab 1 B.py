445cc9990f161cbc0c8a578dfa5051ad5b05c015
katehoey11 <kmhoey@loyola.edu>
2021-01-31 21:13:33 -0500

    share share

--- Lab 1 B.py
+++ Lab 1 B.py
@@ -1,13 +1,31 @@
-#Programmers: Kate Hoey Grace Rapisarda
-#Course: CS151-01
-#January 28 2021
-#Lab 1
-#Program Inputs: Number of ml
-#Program Outputs: Teaspoons and tablespoons
+# Grace Rapisarda and Kate Hoey
 
+# Loyola University Maryland
+# Thursday Jan 28
+# Lab 1
 
+import math
 
-#Formula for converting ml to teaspoons and tablespoons
-#Ask user for ml
-#output number of teaspoons
-#output number of tablespoons
+# output purpose
+print("The purpose of this program is to ask user for mL and convert mL to tsp and tbsp")
+
+# ask user for the input mL
+
+mL = input("Please enter the number of mL: ")
+mL = float(mL)
+
+# equation for mL to tsp
+
+teaspoons = mL/5
+
+# compute the teaspoons
+
+print("The number of teaspoons is: ", teaspoons)
+
+# equation for tablespoons
+
+tablespoons = teaspoons/3
+
+# compute the tablespoons
+
+print("The number of tablespoons is: ", tablespoons)
